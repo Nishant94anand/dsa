fbe885ce3cf47d036e99ff04eaa8aceae90a669c
Nishant Anand <anandnishant@microsoft.com>
2025-10-26 00:53:39 +0530

    Circular Linked List files

---
+++ LinkedList/src/circularLinkedListPackage/CircularLinkedListActions.java
@@ -0,0 +1,13 @@
+package circularLinkedListPackage;
+
+import linkedListUtilityPackage.LinkedListHelper;
+import linkedListUtilityPackage.SinglyLinkedListNode;
+
+public class CircularLinkedListActions {
+
+	public static void main(String[] args) {
+		SinglyLinkedListNode<Integer> head = LinkedListHelper.createIntegerCircularLinkedListFromInput();		
+		
+	}
+
+}
--- LinkedList/src/linkedListUtilityPackage/LinkedListHelper.java
+++ LinkedList/src/linkedListUtilityPackage/LinkedListHelper.java
@@ -98,5 +98,75 @@ public class LinkedListHelper {
 		
 		return linkedList;
 	}
+	
+	public static SinglyLinkedListNode<Integer> createIntegerCircularLinkedListFromInput() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("==> Enter Linked List (e.g., 12 8 7): ");
+		SinglyLinkedListNode<Integer> linkedList = null;
+		String line = sc.nextLine();
+
+		if (line.length() > 0) {
+			String[] ll1String = line.split(" ");
+			List<Integer> ll1List = new ArrayList<>();
+
+			for (String str : ll1String) {
+				ll1List.add(Integer.parseInt(str));
+			}
+
+			linkedList = createSinglyLinkedList(ll1List, true);
+		}
+
+		printCircularLinkedList(linkedList);
+		
+		return linkedList;
+	}
+	
+	public static void printCircularLinkedList(SinglyLinkedListNode<Integer> head)
+	{
+		if (head == null) {
+			System.out.println("null");
+			return;
+		}
+		
+		SinglyLinkedListNode<Integer> curr = head;
+		
+		StringBuilder sb = new StringBuilder();
+		do {
+			sb.append(curr.value + " -> ");
+			curr = curr.next;
+		} while (curr != head);
+		
+		sb.append("back to head -> " + head.value);
+		System.out.println("==> Linked List: " + sb.toString());
+	}
+	
+	private static <T> SinglyLinkedListNode<T> createSinglyLinkedList(List<T> list, boolean createCircular) {
+		if (list == null || list.size() == 0) {
+			return null;
+		}
+		
+		SinglyLinkedListNode<T> rootNode = null;
+		SinglyLinkedListNode<T> prev = null;
+		
+		for (int i=0; i<list.size(); ++i) {
+			T value = list.get(i);
+			SinglyLinkedListNode<T> node = new SinglyLinkedListNode<>(value);
+			
+			if (rootNode == null) {
+				rootNode = node;
+			} else {
+				prev.next = node;
+			}
+			
+			prev = node;
+			
+			if (i == list.size() - 1 && createCircular) {
+				node.next = rootNode;
+			}
+		}
+		
+		return rootNode;
+	}
 
 }
